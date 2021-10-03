ed8cfc6a244cdc76a640e8107709ee69d004159a
Administrator <197026193@qq.com>
2021-10-03 10:28:34 +0800

    现代配方

--- 260/demo.py
+++ 260/demo.py
@@ -4,3 +4,11 @@ class Master(object):
 
     def make_cake(self):
         print(self.kongfu+"的方式制作了一个煎饼果子")
+
+
+class School(object):
+    def __init__(self):
+        self.kongfu = "现代煎饼果子配方"
+
+    def make_cake(self):
+        print(self.kongfu+"的方式制作了一个煎饼果子")
