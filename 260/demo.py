0922a8675c5e728c671a799c527a18045638de4e
Administrator <197026193@qq.com>
2021-10-03 10:41:18 +0800

    猫式配方

--- 260/demo.py
+++ 260/demo.py
@@ -4,7 +4,8 @@ class Master(object):
 
     def make_cake(self):
         print(self.kongfu + "的方式制作了一个煎饼果子")
-
+    def dayandai(self):
+        print('大')
 
 class School(object):
     def __init__(self):
@@ -12,13 +13,20 @@ class School(object):
 
     def make_cake(self):
         print(self.kongfu + "的方式制作了一个煎饼果子")
-
+    def xiaoyandai(self):
+        print('小')
 
 class Prentice(School, Master):
-    pass
+    def __init__(self):
+        self.kongfu = "猫式煎饼果子配方"
+
+    def make_cake(self):
+        print(self.kongfu + "的方式制作了一个煎饼果子")
 
 
 if __name__ == '__main__':
     damao = Prentice()
     print(damao.kongfu)
     damao.make_cake()
+    damao.dayandai()
+    damao.xiaoyandai()
