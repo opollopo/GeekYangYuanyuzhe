e2faf72cb9883308e54a303b8e59647aa02e140e
opollopo <271310065@qq.com>
2021-10-16 13:41:54 +0800

    263密码验证程序,种类验证

--- 263/main.py
+++ 263/main.py
@@ -4,6 +4,13 @@
 # 3、字符不能连续重复3次及以上
 def pass_lens(p):
     return 8 <= len(p) <= 16
-print('1'.isdigit())
-print(''.isalpha())
-for i in range():
+# print('1'.isdigit())
+# print(''.isalpha())
+
+def pass_char(p:str):
+    # 定义3个标志，n,a,t分别表示是否包含数字，字母，特殊字符，如果值是1表示包含，否则不包含
+    n,a,t = 0,0,0
+    for i in p:
+
+
+
