85c72c6f031fac1ec0583222a9cd679898aeac97
opollopo <271310065@qq.com>
2021-11-14 13:51:04 +0800

    猜数字代码提交

--- 401/app.py
+++ 401/app.py
@@ -2,6 +2,8 @@ import flask
 from flask import request, render_template, session, redirect
 import checkor
 import sqlite3
+import random
+import os
 
 app = flask.Flask(__name__, static_url_path='')
 app.secret_key = "fwefewfew16162"
@@ -61,6 +63,51 @@ def pwdCheck(a, p):
     if not checkor.pass_repeat(p):
         a = '字符不能连续重复3次及以上'
     return a
+
+@app.route("/create",methods=["post"])
+def creat_room():
+    """生成目标数字，创建房间号"""
+    room_num = str(random.randint(1, 20))
+    target_num = str(random.randint(1, 200))
+    r_t_n = room_num + " " + target_num
+    room=[]
+    with open("room_tag.txt", "r") as fr:
+        while 1:
+            room_tag_data = fr.readline()
+            if room_tag_data:
+                x, y = room_tag_data.split(" ")
+                room.append([x,y])
+            else:
+                break
+    with open("room_tag.txt", "a") as f:
+        f.write(r_t_n + "\n")
+
+    # return "创建的房间号为：%s,生成目标数字是%s" % (room_num, target_num)
+    # 返回模板文件一个列表，房间号，目标数字，
+    return render_template("main.html",msg2=room)
+
+
+@app.route("/join", methods=["post"])
+def online_public():
+    """加入房间"""
+    # 获取房间号
+    room_number = request.form.get("room_num")
+    # 判断文件是否存在
+    res = os.path.exists("room_tag.txt")
+    if res:
+        # 读取room_tag.txt这个文件
+        with open("room_tag.txt", "r") as f:
+            while 1:
+                room_tag_data = f.readline()
+                if room_tag_data:
+                    x, y = room_tag_data.split(" ")
+                    y = y.replace('\n', '')
+                    if room_number == x:
+                        return render_template("guess.html", v_y=y)
+            else:
+                return render_template("main.html",m="房间号不存在")
+    else:
+        return render_template("main.html",m="房间号不存在，请创建房间号")
 
 
 cx = None
